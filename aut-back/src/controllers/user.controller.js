10ff39401a7eb3df2efc5ff1656bad2092b04862
NoeAlejandroRodriguezMoto <alxg5516@gmail.com>
2024-02-20 20:30:26 -0600

    Agrega dependencia jsonwebtoken y implementa autenticación de usuarios

--- aut-back/src/controllers/user.controller.js
+++ aut-back/src/controllers/user.controller.js
@@ -1,5 +1,6 @@
 import { User } from '../model/user.model.js';
 import { validationResult } from 'express-validator';
+import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
 
 
@@ -13,7 +14,7 @@ export async function registerUser(req, res) {
         const user = await User.create(req.body);
         res.json(user);
     } catch (error) {
-        res.status(500).json({ message: 'Error registering user', error: error.message });
+        res.status(500).json({ message: 'Error interno del servidor de registro de usuarios', error: error.message });
     }
 }
 
@@ -27,16 +28,18 @@ export async function loginUser(req, res) {
         const { email, password } = req.body;
         const user = await User.findOne({ where: { email } });
         if (!user) {
-            res.status(404).json({ message: 'User not found' });
+            res.status(404).json({ message: 'Usuario no encontrado' });
             return;
         }
         const match = await bcrypt.compare(password, user.password);
         if (!match) {
-            res.status(401).json({ message: 'Invalid password' });
+            res.status(401).json({ message: 'Contraseña no invalida' });
             return;
         }
-        res.json(user);
+
+        const token = jwt.sign({ id: user.id, email: user.email }, 'algotranqui', { expiresIn: '6h' });
+        res.json({ message: 'Usuario logueado con éxito', token });
     } catch (error) {
-        res.status(500).json({ message: 'Error logging in', error: error.message });
+        res.status(500).json({ message: 'Error interno del servidor de inicio de sesión de usuarios', error: error.message });
     }
 }
